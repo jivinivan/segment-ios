3f72e711b021d8c177df059ddefb20f1f5fa3fd0
jakedunahee <jake@getambassador.com>
2016-05-24 16:02:35 -0400

    Tried adding runwith keys function

--- Pod/Classes/SEGAmbassasdorIntegration.m
+++ Pod/Classes/SEGAmbassasdorIntegration.m
@@ -11,6 +11,14 @@
 
 @implementation SEGAmbassadorIntegration
 
+- (instancetype)initWithSettings:(NSDictionary *)settings {
+    NSString *sdkToken = settings[@"sdkToken"];
+    NSString *uid = settings[@"uid"];
+    
+    [AmbassadorSDK runWithUniversalToken:sdkToken universalID:uid];
+    NSLog(@"Testing");
+}
+
 - (void)identify:(SEGIdentifyPayload *)payload {
     
 }
